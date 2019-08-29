cb653e9afef58265026f3ff735d687afe3f60d30
chefboyrdeuce <jpldeuce@gmail.com>
2019-08-28 23:40:46 -0400

    added button components and more

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-const Display = () => {
-  return <div>{/* Display any props data here */}</div>;
+const Display = (props) => { // "Display is a function - "
+  console.log("Display", props, props.number);
+  return <div className="display">
+            {/* Display any props data here */}
+            {props.number}
+          </div>;
 };
+
+export default Display;
+
